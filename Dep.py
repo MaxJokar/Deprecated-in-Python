49fd384f036f564478724b1e89ab94bcb8b297f4
email <mmb2020max@gmail.com>
2022-07-10 20:15:47 +0200

    used Black

--- Dep.py
+++ Dep.py
@@ -13,13 +13,13 @@ you can use the @deprecated decorator:
 from deprecated import deprecated
 
 
-@deprecated(version='1.2.1', reason="You should use another function")
+@deprecated(version="1.2.1", reason="You should use another function")
 def some_old_function(x, y):
     return x + y
 
 
 class SomeClass(object):
-    @deprecated(version='1.3.0', reason="This method is deprecated")
+    @deprecated(version="1.3.0", reason="This method is deprecated")
     def some_old_method(self, x, y):
         return x + y
 
@@ -30,21 +30,10 @@ obj.some_old_method(5, 8)
 
 
 #  OutPut:
-    # $ python hello.py
-    # hello.py:15: DeprecationWarning: Call to deprecated function (or staticmethod) some_old_function.
-    # (You should use another function) -- Deprecated since version 1.2.0.
-    #   some_old_function(12, 34)
-    # hello.py:17: DeprecationWarning: Call to deprecated method some_old_method.
-    # (This method is deprecated) -- Deprecated since version 1.3.0.
-    #   obj.some_old_method(5, 8)
-
-
-
-
-
-
-
-
-
-
-
+# $ python hello.py
+# hello.py:15: DeprecationWarning: Call to deprecated function (or staticmethod) some_old_function.
+# (You should use another function) -- Deprecated since version 1.2.0.
+#   some_old_function(12, 34)
+# hello.py:17: DeprecationWarning: Call to deprecated method some_old_method.
+# (This method is deprecated) -- Deprecated since version 1.3.0.
+#   obj.some_old_method(5, 8)
